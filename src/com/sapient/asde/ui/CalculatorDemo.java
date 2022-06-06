3d82fbb4b6c0d8b55c43698da021360d27268d82
pratimk999 <pratimkundu15@gmail.com>
2022-06-06 16:56:14 +0530

    provided subtract method

--- src/com/sapient/asde/ui/CalculatorDemo.java
+++ src/com/sapient/asde/ui/CalculatorDemo.java
@@ -6,5 +6,6 @@ public class CalculatorDemo {
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.println("10.0 + 27.67 is : " + calculator.add(10.0, 27.67));
+        System.out.println("27.67 - 10.0 is : " + calculator.add(27.67,10.0));
     }
 }
